7a68a3b14b0e496b92030bb9801cf922b32f5481
Richard <tuharsky.richard@gmail.com>
2021-09-28 22:15:41 +0200

    Little bit done

---
+++ js/GameView.js
@@ -0,0 +1,13 @@
+export default class GameView {
+
+
+    constructor() {
+        console.log("init gameview")
+    }
+
+    updateBoard(){
+        console.log(game.board);
+        console.log("This is a board within GameView")
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-import Game from "./Game.js"
+import Game from "./Game.js";
+import GameView from "./GameView.js"
 
 let game = new Game();
+let gameView = new GameView();
+gameView.updateBoard(game);
 
 
 console.log("My turn is", game.turn);
 game.nextTurn();
 console.log("My turn is", game.turn);
-game.makeMove(3)
+game.makeMove(3);
+console.log(game.board);
+game.nextTurn();
+game.makeMove(8);
+console.log(game.board)
